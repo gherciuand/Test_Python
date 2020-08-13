9c0bd20eff37fcf7c5c61e44805080fd129278c0
gherciuand <68415760+gherciuand@users.noreply.github.com>
2020-08-13 11:08:09 +0300

    Update HW_9_Class_DefPropMethod.py
    add coments

--- HomeWork_9/HW_9_Class_DefPropMethod.py
+++ HomeWork_9/HW_9_Class_DefPropMethod.py
@@ -3,6 +3,8 @@ class Student:
     spec = None
     grades = []
 
+# define propreties of class Student
+
     def setPropr(self, n, s, g):
         self.name = n
         self.spec = s
@@ -12,11 +14,15 @@ class Student:
             else:
                 print("Ooops, wrong value")
 
+# define methods of class Student (sayHi)
+
     def sayHi(self):
         print(f'Hi! my name is {self.name}. I learn {self.spec}\n'
               f'My grades is:')
         for i in range(len(self.grades)):
             print(f'>> {self.grades[i]:10f}')
+
+# define methods of class Student (Average Grade)
 
     def calcAvgGrade(self):
         s = 0
@@ -24,12 +30,20 @@ class Student:
             s += self.grades[i]
         print(f'Average grade of {self.name} is {s / len(self.grades):2f}')
 
+        #V2--AvgGride
+        #print(f'Average grade of {self.name} is {sum(self.grades) / len(self.grades):2f}')
+
+# assigment variable to class
 
 s1 = Student()
 s2 = Student()
 
+#input preprieties of variable
+
 s1.setPropr('Ion', 'MIT', [10.0, 9.65, 8, 6.58, 7.5])
 s2.setPropr('Maria', 'Philology', [7, 8.5, 9.6, 8.3])
+
+#show variables functions
 
 s1.sayHi()
 s1.calcAvgGrade()
