29b86548646535980ab595dca99ce98c5a2ce7af
gherciuand <68415760+gherciuand@users.noreply.github.com>
2020-08-25 02:08:18 +0300

    add HW_11

---
+++ HomeWork_11/HW_11_FileOrderFromMenu.py
@@ -0,0 +1,29 @@
+from os import path
+
+menu_file = "menu.txt"
+order_file = "order.txt"
+
+if path.exists(menu_file) and path.exists(order_file):
+    print (f'You can place the order')
+    menu = open(menu_file, "r")
+    order = open(order_file, "r")
+    list_line_m = menu.readlines()
+    list_line_o = order.readlines()
+    list_menu=[]
+    list_order=[]
+    total_price=[]
+    for i in range(len(list_line_m)):
+        list_menu.append(list_line_m[i].strip().split("-"))
+        for j in range(len(list_line_o)):
+            list_order.append(list_line_o[j].strip().split("-"))
+            if list_order[j][0]==list_menu[i][0]:
+                tp = int(list_order[j][1])*int(list_menu[i][1])
+                total_price.append(tp)
+                print(f'You ordered: '
+                  f'{list_order[j][0]:>15} - quantity {str(list_order[j][1]):2} pcs,'
+                  f' price {str(int(list_order[j][1])*int(list_menu[i][1])):>4} {list_menu[i][2]}')
+    print(f'Total price:{"-"*40}  {sum(total_price):>4} MDL')
+else:
+    print(f'You can NOT place the order, some files meesing')
+
+
