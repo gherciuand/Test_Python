df59e594cf1fdd8cd01ff20d79f84d3bb8a3d2b6
gherciuand <68415760+gherciuand@users.noreply.github.com>
2020-08-25 02:29:18 +0300

    updated (output-file)
    add outpute file
    write function

--- HomeWork_11/HW_11_FileOrderFromMenu.py
+++ HomeWork_11/HW_11_FileOrderFromMenu.py
@@ -2,11 +2,14 @@ from os import path
 
 menu_file = "menu.txt"
 order_file = "order.txt"
+output_file = "Total_order.txt"
 
 if path.exists(menu_file) and path.exists(order_file):
     print (f'You can place the order')
     menu = open(menu_file, "r")
     order = open(order_file, "r")
+    output_order = open(output_file, "w")
+    output_order.write(f'Your order is:\n\n')
     list_line_m = menu.readlines()
     list_line_o = order.readlines()
     list_menu=[]
@@ -22,7 +25,11 @@ if path.exists(menu_file) and path.exists(order_file):
                 print(f'You ordered: '
                   f'{list_order[j][0]:>15} - quantity {str(list_order[j][1]):2} pcs,'
                   f' price {str(int(list_order[j][1])*int(list_menu[i][1])):>4} {list_menu[i][2]}')
+                output_order.write(
+                  f'{list_order[j][0]:15} - quantity {str(list_order[j][1]):2} pcs,'
+                  f' price {str(int(list_order[j][1])*int(list_menu[i][1])):>4} {list_menu[i][2]}\n')
     print(f'Total price:{"-"*40}  {sum(total_price):>4} MDL')
+    output_order.write(f'\nTotal price:{"-"*27}  {sum(total_price):>4} MDL')
 else:
     print(f'You can NOT place the order, some files meesing')
 
